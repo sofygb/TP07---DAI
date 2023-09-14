4cefa72fddecb4bb7b255e8ee112648e24695563
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-14 12:03:49 -0300

    getcomida&receta

--- tp07/Consultas.js
+++ tp07/Consultas.js
@@ -1,11 +1,20 @@
-const key = "?apiKey=0cb9a80046de48228264993c6810fd45&"
+const key = "apiKey=0cb9a80046de48228264993c6810fd45"
 
-const API1 = "https://api.spoonacular.com/recipes/716429/information"
+const API1 = "https://api.spoonacular.com/recipes/716429/information?"
+
+const API2 = "https://api.spoonacular.com/recipes/716429/information?includeNutrition=false&"
 
 //https://api.spoonacular.com/recipes/716429/information?apiKey=0cb9a80046de48228264993c6810fd45&
+//https://api.spoonacular.com/recipes/716429/information?includeNutrition=false&apiKey=0cb9a80046de48228264993c6810fd45
 
 export const getPlatos = async () => {
     const resp = await fetch(`${API1}${key}`);
+    const data = await resp.json();
+    return data;
+}
+
+export const getInformacionReceta = async () => {
+    const resp = await fetch(`${API2}${key}`);
     const data = await resp.json();
     return data;
 }
--- tp07/components/Home.jsx
+++ tp07/components/Home.jsx
@@ -1,31 +1,55 @@
 import { useEffect, useState } from "react"
-import { getPlatos } from "../Consultas"
+import { getPlatos, getInformacionReceta } from "../Consultas"
 
 export const Home = () => {
     const [platos, setPlatos] = useState(null)
+    const [recetas, setRecetas] = useState(null)
 
     const traerPlatos = async () => {
         const data = await getPlatos()
         console.log(data)
         setPlatos([data])
     }
+    const traerRecetas = async () => {
+        const data = await getInformacionReceta()
+        console.log(data)
+        setRecetas([data])
+    }
     
     useEffect(() => {
         traerPlatos()
+        traerRecetas()
+        console.log(platos)
     }, [])
     
-    console.log(platos)
     return (
         <>
         <h5>Menú:</h5>
         {
-            platos != null && platos.map((plato) => {
-                <>
-                <img src={plato.image}/>
-                <p>Nombre: {plato.title}</p>
-                <p>Precio: {plato.pricePerServing}</p>
-                </>
-            })
+            platos != null && platos.map((plato) => 
+                <div id={plato.id}>
+                    <img src={plato.image}/>
+                    <p>Nombre: {plato.title}</p>
+                    <p>Precio: ${plato.pricePerServing}</p>
+                </div>
+            )
+        }
+        {
+            recetas != null && recetas.map((receta) => 
+                <div id={receta.id}>
+                    <p>Nombre: {receta.title}</p>
+                    <p>Porciones por plato: ${receta.servings}</p>
+                    <p>Precio: ${receta.pricePerServing}</p>
+                    <p>Tiempo de preparación: {receta.readyInMinutes} minutos</p>
+                    <p>HealthScore: {receta.healthScore}</p>
+                    <p>Libre de lactosa: {receta.dairyFree}</p>
+                    <p>Keto: {receta.ketogenic}</p>
+                    <p>Apto para celíacos: {receta.glutenFree}</p>
+                    <p>Apto para veganos: {receta.vegan}</p>
+                    <p>Apto para vegetarianos: {receta.vegetarian}</p>
+                    <p>Platos para: {receta.dishTypes.map((comidas) => comidas)}</p>
+                </div>
+            )
         }
         </>
     )
